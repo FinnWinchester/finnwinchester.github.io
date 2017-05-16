94cc62f6e7f3cb4d71184a1425d751fce29f87b9
Finn Winchester <finnwinchester@Finn-Winchester.local>
2017-05-16 16:25:03 +0200

    Removed unnecessary code

--- app/scenes/Home/components/HomeBody/index.jsx
+++ app/scenes/Home/components/HomeBody/index.jsx
@@ -4,28 +4,7 @@ import MySkills from './components/MySkills';
 import MyProjects from './components/MyProjects';
 
 class HomeHeader extends React.Component {
-  constructor(props) {
-    super(props);
-
-    let _get_age = birthday => {
-      var ageDifMs = Date.now() - birthday.getTime();
-      var ageDate = new Date(ageDifMs); // miliseconds from epoch
-      return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
-
-    this.state = {
-      age: _get_age(new Date(1990, 7, 29))
-    };
-  }
-
   render() {
-    let skillImageStyle = {
-      width: '100%'
-    };
-    let skillHalfImageStyle = {
-      width: '50%',
-      float: 'left'
-    };
     return (
       <div className='home-body'>
         <div className='zebra'>
