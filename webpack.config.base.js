73e7eaec42e61e00b5db610c483f328fbc4e1f81
Finn Winchester <rubensillero90@gmail.com>
2022-03-26 11:51:48 +0100

    Build.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'docs'),
     publicPath: '/'
   },
   plugins: [
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,6 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const baseConfig = require('./webpack.config.dev');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   devtool: 'hidden-source-map',
