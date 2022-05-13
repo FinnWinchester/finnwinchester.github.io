848610e692055a323c77f1db1194510d23f326e8
FinnWinchester <4563348+FinnWinchester@users.noreply.github.com>
2022-05-13 16:37:17 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,18 +12,6 @@ export default function Footer() {
   return (
     <FooterWrapper>
       <FooterRow>
-        <FooterRowTitle>Contact</FooterRowTitle>
-
-        <FooterRowText>
-          If you want to contact me you can do it throught my e-mail address{' '}
-          <a href="mailto:rubensillero90@gmail.com" target="_blank" rel="noopener noreferrer" className="black-link">
-            rubensillero90@gmail.com
-          </a>
-          .
-        </FooterRowText>
-      </FooterRow>
-
-      <FooterRow>
         <FooterRowTitle>What am I up to</FooterRowTitle>
 
         <FooterRowText>Afterworks, learn, teach. Enjoy life.</FooterRowText>
